f786bab791a5262b21f3dfcf70e70facd21aeeae
BakeCoCo <58055835+BakeCoCo@users.noreply.github.com>
2019-12-08 13:54:56 +0900

    Initial commit

---
+++ orange/src/orange/managerVO.java
@@ -0,0 +1,50 @@
+package orange;
+
+public class managerVO {
+
+	int won;
+	String menu;
+	int ea;
+	int idx;
+	String menulist;
+	int money;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getMenulist() {
+		return menulist;
+	}
+	public void setMenulist(String menulist) {
+		this.menulist = menulist;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public int getWon() {
+		return won;
+	}
+	public void setWon(int won) {
+		this.won = won;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public int getEa() {
+		return ea;
+	}
+	public void setEa(int ea) {
+		this.ea = ea;
+	}
+	
+	
+}
